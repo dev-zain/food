e5868c3f58f50533f13afef88657851ae50afcd5
zain ali <ibrahimkhan35821@gmail.com>
2022-09-07 09:56:52 -0700

    vendor registration

---
+++ vendors/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms
+from .models import Vendor
+
+class VendorForm(forms.ModelForm):
+    class Meta:
+        model = Vendor
+        fields = ['vendor_name','vendor_license']
