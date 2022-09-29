157574af9b0c83eb554dc2fc3fc5c893ef78e356
zain ali <ibrahimkhan35821@gmail.com>
2022-09-29 09:23:22 -0700

    food CRUD operation

--- accounts/validators.py
+++ accounts/validators.py
@@ -3,6 +3,6 @@ import os
 
 def allow_only_images_validator(value):
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['png','jpg','jpeg']
+    valid_extensions = ['.png','.jpg','.jpeg']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file type, supported file types are ' + str(valid_extensions))
--- vendors/urls.py
+++ vendors/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('menu-builder/add/category',views.add_category,name='add_category'),
     path('menu-builder/category/edit/<int:pk>/',views.edit_category,name='edit_category'),
     path('menu-builder/category/delete/<int:pk>/',views.delete_category,name='delete_category'),
+
+    # food crud
+    path('menu-builder/add/food',views.add_food,name='add_food'),
+    path('menu-builder/food/edit/<int:pk>/',views.edit_food,name='edit_food'),
+    path('menu-builder/food/delete/<int:pk>/',views.delete_food,name='delete_food'),
 ]
